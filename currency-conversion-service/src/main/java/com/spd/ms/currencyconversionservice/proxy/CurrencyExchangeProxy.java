39ee01c4da3fbf53e8d0c04a5ffc2eb90f3e8612
Satish Prasad <spd61185@gmail.com>
2021-02-27 14:37:42 +0530

    currency-conversion-service

---
+++ currency-conversion-service/src/main/java/com/spd/ms/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.spd.ms.currencyconversionservice.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.spd.ms.currencyconversionservice.entity.CurrencyConversion;
+
+
+@FeignClient(name = "currency-exchange",url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion getExchangeValue(@PathVariable String from,@PathVariable String to);
+}
